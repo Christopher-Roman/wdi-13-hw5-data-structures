6ffb864cec67fb887ebe40a96ed279d72b68bb1e
Chris Roman <Christopherroman85@gmail.com>
2018-09-17 22:14:13 -0500

    object-ception

--- js/app.js
+++ js/app.js
@@ -94,8 +94,29 @@ const crazyObject = {
 // 5.
 	console.log(crazyObject.larry.characters[1]);
 
+// Object-ception
 
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
 
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6 = null;
+
+console.log(inception);
 
 
 
