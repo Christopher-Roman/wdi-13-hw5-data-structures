7b9162e52ea737ecc251be698c3365419334af00
Chris Roman <Christopherroman85@gmail.com>
2018-09-17 21:44:47 -0500

    data types

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// console.log('homework');
+
+// I'm pretty sure that I am over thinking this...
+
+// 1. I would create a function with use if/else logic and booleans for the light switch
+//	  const lightSwitch (switch) => {	
+//		if(switch = true) {
+//	console.log('The light is on.')
+//	  }	else {
+//	  	console.log('The light is off');
+//	  }
+//	}
+
+// 2. You would use a string for a user's e-mail address
+//		const userEmail = 'Tophy03@gmail.com'
+
+// 3. I would use an object to represent thsi
+//		const spaceship = {
+// 		hull: 'Spacetanium',
+// 		weapons: 'Laser Blasters',
+// 		grappler: 'Tractor Beam',
+// 		engine: 'Warp Drive'
+// }
+
+// 4. I would use an Array
+	// const gaStudents = ['Chris L', 'Charlotte', 'Greg', 'Andy', 'Chris R', 'Filip', 'Josh', 'Ashley', 'John']
+
+// 5. I would use an array of objects
+	// const gaStudents = [{name: 'Chris L', location: 'Chicago'}, {name: 'Charlotte', location: 'Paris'}, etc...]
+// 6.I would use an array of objects with each with its own array
+	// const gaStudents = [
+		// {name: 'Chris L', location: 'Chicago', favShows: ['some show', 'some other show', 'hey another one']}, 
+		// {name: 'Charlotte', location: 'Paris', favShows: ['anime show', 'some other anime show', 'hey another one']}, 
+		// etc...]
+
+
